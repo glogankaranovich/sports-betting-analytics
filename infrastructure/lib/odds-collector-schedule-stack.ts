493cedbc238d0d5c7293fa4337ce67930f4efd75
Logan Karanovich <gkaranovich3@gmail.com>
2026-02-28 09:49:07 -0500

    Add NCAA sports to odds collector schedules

--- infrastructure/lib/odds-collector-schedule-stack.ts
+++ infrastructure/lib/odds-collector-schedule-stack.ts
@@ -28,7 +28,10 @@ export class OddsCollectorScheduleStack extends cdk.Stack {
       { key: 'americanfootball_nfl', name: 'NFL' },
       { key: 'baseball_mlb', name: 'MLB' },
       { key: 'icehockey_nhl', name: 'NHL' },
-      { key: 'soccer_epl', name: 'EPL' }
+      { key: 'soccer_epl', name: 'EPL' },
+      { key: 'basketball_mens-college-basketball', name: 'NCAAMBB' },
+      { key: 'basketball_womens-college-basketball', name: 'NCAAWBB' },
+      { key: 'football_college-football', name: 'NCAAFB' }
     ];
 
     // Odds collection - every 4 hours for each sport (staggered by 15 minutes)
@@ -38,6 +41,9 @@ export class OddsCollectorScheduleStack extends cdk.Stack {
       'cron(30 */4 * * ? *)',  // MLB: 00:30, 04:30, 08:30, 12:30, 16:30, 20:30 UTC
       'cron(45 */4 * * ? *)',  // NHL: 00:45, 04:45, 08:45, 12:45, 16:45, 20:45 UTC
       'cron(50 */4 * * ? *)',  // EPL: 00:50, 04:50, 08:50, 12:50, 16:50, 20:50 UTC
+      'cron(55 */4 * * ? *)',  // NCAAMBB: 00:55, 04:55, 08:55, 12:55, 16:55, 20:55 UTC
+      'cron(5 */4 * * ? *)',   // NCAAWBB: 00:05, 04:05, 08:05, 12:05, 16:05, 20:05 UTC
+      'cron(10 */4 * * ? *)',  // NCAAFB: 00:10, 04:10, 08:10, 12:10, 16:10, 20:10 UTC
     ];
 
     sports.forEach((sport, index) => {
@@ -60,6 +66,9 @@ export class OddsCollectorScheduleStack extends cdk.Stack {
       'cron(30 */8 * * ? *)',  // MLB: 00:30, 08:30, 16:30 UTC
       'cron(45 */8 * * ? *)',  // NHL: 00:45, 08:45, 16:45 UTC
       'cron(50 */8 * * ? *)',  // EPL: 00:50, 08:50, 16:50 UTC
+      'cron(55 */8 * * ? *)',  // NCAAMBB: 00:55, 08:55, 16:55 UTC
+      'cron(5 */8 * * ? *)',   // NCAAWBB: 00:05, 08:05, 16:05 UTC
+      'cron(10 */8 * * ? *)',  // NCAAFB: 00:10, 08:10, 16:10 UTC
     ];
 
     sports.forEach((sport, index) => {
